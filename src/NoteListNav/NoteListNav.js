4d32288019da11a88aa007fe558d22f0114c4753
mertbagt <73559781+mertbagt@users.noreply.github.com>
2020-12-14 22:31:56 -0500

    updated NoteListNav

--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 import CircleButton from '../CircleButton/CircleButton'
 import ApiContext from '../ApiContext'
 import { countNotesForFolder } from '../notes-helpers'
